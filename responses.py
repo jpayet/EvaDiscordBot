81dd2bfe4d31a46e36f7dc68e4969c4a128616bc
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-03-15 16:13:41 +0100

    comment

--- responses.py
+++ responses.py
@@ -3,7 +3,7 @@ def handle_response(message) -> str:
     if message == 'hello':
         return 'Hey there !'
 
-    #à changer et utiliser les bot au lieu du client
+    # à changer et utiliser les bot au lieu du client
     if message == '!help':
         return "`Vous avez besoin d'aide... je suis là !`"
 
