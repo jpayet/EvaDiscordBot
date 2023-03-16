d552fcd70986f02812119472067b1086af5ba3f1
Jean-François Payet <100703277+jpayet@users.noreply.github.com>
2023-03-16 13:44:55 +0100

    global and user commands

--- bot.py
+++ bot.py
@@ -1,42 +1,61 @@
 import os
 import discord
 import responses
+from discord import app_commands
+from discord.ext import commands
 
 from dotenv import load_dotenv
 load_dotenv()
 
 
 def run_discord_bot():
+    #définition des droits du bot
     intents = discord.Intents.all()
-    client = discord.Client(intents=intents)
+    bot = commands.Bot(command_prefix="!", intents=intents)
 
-    @client.event
+    @bot.event
     async def on_ready():
-        print(f'{client.user} est en marche !')
+        print(f'{bot.user} est en marche !')
+        try:
+            synced = await bot.tree.sync()
+        except Exception as e:
+            print(e)
 
-    @client.event
+    @bot.tree.command(name="say")
+    @app_commands.describe(thing_to_say="What should I say ? ")
+    async def say(interaction: discord.Interaction, thing_to_say: str):
+        await interaction.response.send_message(f"{interaction.user.name}! said : '`{thing_to_say}`")
+
+    @bot.event
     async def on_message(message):
         response = responses.handle_response(message.content.lower())
 
-        if message.author == client.user:
+        if message.author == bot.user:
             return
 
         if response is not None:
             await message.channel.send(response)
 
-    @client.event
+        await bot.process_commands(message)
+
+    @bot.event
     async def on_member_join(member):
-        bienvenue_channel: discord.TextChannel = client.get_channel(1085268089257607278)
+        bienvenue_channel: discord.TextChannel = bot.get_channel(1085268089257607278)
         await bienvenue_channel.send(content=f"Ravi de te voir parmi nous {member.display_name}")
-        role = discord.utils.get(member.guild.roles, name='Recrue')
+        role = discord.utils.get(member.guild.roles, name='New')
         await member.add_roles(role)
 
-    @client.event
+    @bot.event
+    async def on_member_remove(member):
+        print(f'bye {member}')
+        pass
+
+    @bot.event
     async def on_raw_reaction_add(payload):
         message_id = payload.message_id
         if message_id == 1085189648025927730:
             guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
 
             if payload.emoji.name == '🎮':
                 role = discord.utils.get(guild.roles, name='Gaming')
@@ -49,13 +68,29 @@ def run_discord_bot():
                 member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
                 if member is not None:
                     await member.add_roles(role)
+        elif message_id == 1085678888451063849:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
 
-    @client.event
+            if payload.emoji.name == '✅':
+                role = discord.utils.get(guild.roles, name='Membre')
+                role_del = discord.utils.get(guild.roles, name='New')
+            else:
+                role = None
+                role_del = None
+
+            if role is not None and role_del is not None:
+                member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.add_roles(role)
+                    await member.remove_roles(role_del)
+
+    @bot.event
     async def on_raw_reaction_remove(payload):
         message_id = payload.message_id
         if message_id == 1085189648025927730:
             guild_id = payload.guild_id
-            guild = discord.utils.find(lambda g: g.id == guild_id, client.guilds)
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
 
             if payload.emoji.name == '🎮':
                 role = discord.utils.get(guild.roles, name='Gaming')
@@ -68,5 +103,28 @@ def run_discord_bot():
                 member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
                 if member is not None:
                     await member.remove_roles(role)
+        elif message_id == 1085678888451063849:
+            guild_id = payload.guild_id
+            guild = discord.utils.find(lambda g: g.id == guild_id, bot.guilds)
 
-    client.run(os.getenv("TOKEN"))
+            if payload.emoji.name == '✅':
+                role = discord.utils.get(guild.roles, name='New')
+                role_del = discord.utils.get(guild.roles, name='Membre')
+            else:
+                role = None
+                role_del = None
+
+            if role is not None and role_del is not None:
+                member = discord.utils.find(lambda m: m.id == payload.user_id, guild.members)
+                if member is not None:
+                    await member.add_roles(role)
+
+                    await member.remove_roles(role_del)
+
+    @bot.command(name='del')
+    async def delete(ctx, nb_of_message: int):
+        messages = ctx.channel.history(limit=nb_of_message + 1)
+        async for each_message in messages:
+            await each_message.delete()
+
+    bot.run(os.getenv("TOKEN"))
